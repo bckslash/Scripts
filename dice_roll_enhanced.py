0caccab8fd4a37d42a1f0866f766f6ee77a7afbc
Robert <robert.smrek@student.tuke.sk>
2021-02-09 12:14:10 +0100

    dice odds simulator

--- dice_roll_enhanced.py
+++ dice_roll_enhanced.py
@@ -8,9 +8,9 @@ for i in range(rolls):
 	all_dices.append(random.randint(1, dice_size))
 
 temp = 0
-for i in range(1, dice_size + 1):
-	for j in range(rolls):
-		if all_dices[j] == i:
+for dice_number in range(1, dice_size + 1):
+	for i in range(len(all_dices)):
+		if all_dices[i] == dice_number:
 			temp += 1
-	print("Number {} dropped {} times, the odds are {}%".format(i, temp, 100*(temp/rolls)))
+	print("Number {} dropped {} times, the odds are {}%".format(dice_number, temp, 100*(temp/rolls)))
 	temp = 0
